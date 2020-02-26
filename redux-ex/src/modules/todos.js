4ef877b9c3e4d11cf10d18ce2fc143c7b87378af
SeongHwan1001 <limmli04210@gmail.com>
2020-02-26 10:10:59 +0900

    immer 적용

--- redux-ex/src/modules/todos.js
+++ redux-ex/src/modules/todos.js
@@ -1,4 +1,5 @@
 import { createAction, handleActions } from "redux-actions";
+import produce from "immer";
 
 const CHANGE_INPUT = "todos/CHANGE_INPUT"; // 인풋 값을 변경함
 const INSERT = "todos/INSERT"; // 새로운 todo를 등록함
@@ -92,23 +93,27 @@ const initialState = {
 //   }
 // }
 
+// 객체의 구조가 복잡해지거나 객체로 이루어진 배열을 다룰 경우, immer를 사용하면 훨씬 편리하게 상태를 관리 할 수 있다는 장점이 있다.
 const todos = handleActions(
   {
-    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
-    [INSERT]: (state, { payload: todo }) => ({
-      ...state,
-      todos: state.todos.concat(todo)
-    }),
-    [TOGGLE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.map(todo =>
-        todo.id === id ? { ...todo, done: !todo.done } : todo
-      )
-    }),
-    [REMOVE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.filter(todo => todo.id !== id)
-    })
+    [CHANGE_INPUT]: (state, { payload: input }) =>
+      produce(state, draft => {
+        draft.input = input;
+      }),
+    [INSERT]: (state, { payload: todo }) =>
+      produce(state, draft => {
+        draft.todos.push(todo);
+      }),
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, draft => {
+        const todo = draft.todos.find(todo => todo.id === id);
+        todo.done = !todo.done;
+      }),
+    [REMOVE]: (state, { payload: id }) =>
+      produce(state, draft => {
+        const index = draft.todos.findIndex(todo => todo.id === id);
+        draft.todos.splice(index, 1);
+      })
   },
   initialState
 );
